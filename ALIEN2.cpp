0cb60848787e0308a09035d6ca305acf5283920e
vidit chopra <51998522+viditchopra1500@users.noreply.github.com>
2020-08-24 14:47:02 +0530

    Update ALIEN2.cpp

--- ALIEN2.cpp
+++ ALIEN2.cpp
@@ -1,4 +1,4 @@
-int main()
+1)int main()
 {fast;
         ll n,k;
         read(n,k);
@@ -25,3 +25,38 @@ int main()
         else
         cout<<n<<" "<<min(dp[n-1][0],dp[n-1][1]);
     }
+
+2)push dp
+void solve(){
+    ll n,k;
+    read(n,k);
+    ll a[n],b[n];
+    read(a,n);
+    read(b,n);
+    ll dp[n][2];
+    // dp[i][j] is the minimum number of people alien has to pass 
+    // if she is at station i and train j;
+    ini(dp,MOD);
+    dp[0][0]=a[0];
+    dp[0][1]=b[0];
+    forn(i,n-1){
+        forn(j,2){
+            if(j==0){
+                dp[i+1][0]=min(dp[i][0]+a[i+1],dp[i+1][0]);
+                dp[i+1][1]=min(dp[i][0]+b[i+1]+b[i],dp[i+1][1]);
+            }
+            else
+            {
+                dp[i+1][1]=min(dp[i][1]+b[i+1],dp[i+1][1]);
+                dp[i+1][0]=min(dp[i][1]+a[i+1]+a[i],dp[i+1][0]);
+            }
+        }
+    }
+    forn(i,n){
+            if(i==n-1||(dp[i+1][0]>k&&dp[i+1][1]>k)){
+                cout<<i+1<<" "<<min(dp[i][0],dp[i][1]);
+                return;
+            }
+    }
+}
+
